ee0c753b1fe0a736b078c63a854e1ce6b09a2813
Kamil <cgstyle@gmail.com>
2016-03-21 14:27:13 +0100

    added connectionstring for MySQL database

---
+++ PhotoshopWebsite/DatabaseTier/Login.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace PhotoshopWebsite.DatabaseTier
+{
+    public class Login
+    {
+        //Get the MySQL connection Singleton Instance
+        private ConnectionSingleton connectionSingleton = ConnectionSingleton.GetSingleton();
+
+        // Create a new MySQL connections
+        private MySqlConnection mysqlConnection;
+        private MySqlCommand myCommand = null;
+        private string result = "";
+
+        public Login()
+        {
+            // get the mysqlconnection singleton
+            mysqlConnection = connectionSingleton.getSqlConnection();
+        }
+
+        /// <summary>
+        /// This method returns a true false boolean when a user trys to log in
+        /// </summary>
+        /// <param name="emailadres"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public bool loginUser(string emailadres, string password)
+        {
+
+            try
+            {
+                //System.Windows.Forms.MessageBox.Show("Opening SQL connection");
+
+                myCommand = new MySqlCommand(null, mysqlConnection);
+                myCommand.CommandText = "getUserPassword";
+                myCommand.CommandType = CommandType.StoredProcedure;
+
+                myCommand.Parameters.Add("@p_account_Email", MySqlDbType.VarChar).Value = emailadres;
+                myCommand.Parameters.Add("@p_password", MySqlDbType.VarChar).Value = password;
+
+
+                myCommand.Parameters.Add("@p_passed", MySqlDbType.VarChar);
+                myCommand.Parameters["@p_passed"].Direction = ParameterDirection.Output;
+
+                mysqlConnection.Open();
+                myCommand.ExecuteNonQuery();
+
+                if (myCommand.Parameters["@p_passed"].Value.ToString() == "true")
+                {
+                    return true;
+                }
+
+                return false;
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                myCommand.Connection.Close();
+                mysqlConnection.Close();
+                //System.Windows.Forms.MessageBox.Show("Closed SQL connection");
+            }
+
+            return false;
+
+        }
+    }
+}
--- PhotoshopWebsite/Gui/Login.aspx.cs
+++ PhotoshopWebsite/Gui/Login.aspx.cs
@@ -6,6 +6,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 
 
+
 namespace PhotoshopWebsite
 {
     public partial class Login : System.Web.UI.Page
@@ -14,23 +15,31 @@ namespace PhotoshopWebsite
         private String passWord;
         private Boolean Rememberme = false;
         private Boolean LoginSuccess = true;
-        private String loginCode; 
-       
+        private String loginCode;
+        private DatabaseTier.Login login = new DatabaseTier.Login();
+
 
         protected void Page_Load(object sender, EventArgs e)
         {
-           
+            
         }
 
         protected void BtnLogin_Click(object sender, EventArgs e)
         {
             loginName = tbInputEmail.Text;
             passWord = tbInputPassword.Text;
-            if (LoginSuccess)
+            bool result = login.loginUser(loginName, passWord);
+
+            if (result == true)
             {
                 Session["logindata"] = loginName;
                 Response.Redirect("~/Gui/Client/Mainstore.aspx?ReturnPath=" + Server.UrlEncode(Request.Url.AbsoluteUri));
             }
+            else
+            {
+                Response.Write("<script>alert('Wrong emailaddress or password')</script>");
+            }
+
         }
 
         protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
