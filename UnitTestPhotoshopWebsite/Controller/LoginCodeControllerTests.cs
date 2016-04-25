8fc34cc395dfe10d95eca446f8583e5c73bde92c
Loek Delahaye <Loekdelahaye@gmail.com>
2016-04-25 12:04:01 +0200

    default filter added controller test

---
+++ UnitTestPhotoshopWebsite/Controller/LoginCodeControllerTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using PhotoshopWebsite.Controller;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestPhotoshopWebsite.Controller
+{
+    [TestClass]
+    public class LoginCodeControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
