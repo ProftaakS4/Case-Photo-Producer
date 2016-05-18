99668a79f352a59f4dc8676093f39e6445c1ce42
Wulms,Stan S.N.M.M <stan.wulms@student.fontys.nl>
2016-05-18 10:50:53 +0200

    Added procedure to get orders

--- PhotoshopWebsite/DatabaseTier/Order.cs
+++ PhotoshopWebsite/DatabaseTier/Order.cs
@@ -16,9 +16,57 @@ namespace PhotoshopWebsite.DatabaseTier
         private MySqlConnection mysqlConnection;
         private MySqlCommand myCommand = null;
         private string result = "";
+        
         public Order()
         {
+            // get the mysqlconnection singleton
+            mysqlConnection = connectionSingleton.getSqlConnection();
+        }
 
+        /// <summary>
+        /// this method returns a dictionary containing all the purchase data. when user not found, method returns null
+        /// </summary>
+        /// <param name="accountID"></param> id of the logged on account
+        /// <returns></returns>
+        public DataTable getOrders(int accountID)
+        {
+            try
+            {
+                myCommand = new MySqlCommand("getPurchases", mysqlConnection);
+                // input
+                myCommand.CommandType = CommandType.StoredProcedure;
+                myCommand.Parameters.Add("@p_account_ID", MySqlDbType.Int32).Value = accountID;
+                // output
+                myCommand.Parameters.Add("@ID", MySqlDbType.Int32);
+                myCommand.Parameters["@ID"].Direction = ParameterDirection.Output;
+                myCommand.Parameters.Add("@ACCOUNT_ID", MySqlDbType.Int32);
+                myCommand.Parameters["@ACCOUNT_ID"].Direction = ParameterDirection.Output;
+                myCommand.Parameters.Add("@Date", MySqlDbType.Date);
+                myCommand.Parameters["@Date"].Direction = ParameterDirection.Output;
+                myCommand.Parameters.Add("@Status", MySqlDbType.VarChar);
+                myCommand.Parameters["@Status"].Direction = ParameterDirection.Output;
+                // execute query
+                mysqlConnection.Open();
+                //myCommand.ExecuteNonQuery();
+
+                //MySqlDataAdapter da = new MySqlDataAdapter(myCommand);
+                MySqlDataReader dr = myCommand.ExecuteReader(CommandBehavior.CloseConnection);
+                DataTable dt = new DataTable();
+                dt.Load(dr);
+
+                //return datatable
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                myCommand.Connection.Close();
+                mysqlConnection.Close();
+            }
+            return null;
         }
     }
 }
