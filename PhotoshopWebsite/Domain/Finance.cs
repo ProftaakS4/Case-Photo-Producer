2ad38f6c4e7dad6428d13f43c60dc2c14a68b227
Wulms,Stan S.N.M.M <stan.wulms@student.fontys.nl>
2016-05-27 17:17:22 +0200

    Added Finances (Owed Money per photographer)

--- PhotoshopWebsite/Domain/Finance.cs
+++ PhotoshopWebsite/Domain/Finance.cs
@@ -5,14 +5,14 @@ using System.Web;
 
 namespace PhotoshopWebsite.Domain
 {
-    public class OwedMoney
+    public class Finance
     {
         public int ID { get; set; }
         public String FirstName { get; set; }
         public String LastName { get; set; }
         public int Money { get; set; }
 
-        public OwedMoney(int id, string firstName, string lastName, int money)
+        public Finance(int id, string firstName, string lastName, int money)
         {
             this.ID = id;
             this.FirstName = firstName;
