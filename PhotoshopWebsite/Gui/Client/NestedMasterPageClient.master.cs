867906f5c43aced5427bdb99cf3ece53e9a33561
Charles-Foster-Kane <ramonj@live.nl>
2016-05-31 12:32:45 +0200

    faal

---
+++ PhotoshopWebsite/Gui/Client/NestedMasterPageClient.master.cs
@@ -0,0 +1,137 @@
+﻿using PhotoshopWebsite.Controller;
+using PhotoshopWebsite.DatabaseTier;
+using PhotoshopWebsite.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PhotoshopWebsite.Gui.Client
+{
+    [ExcludeFromCodeCoverage]
+    public partial class NestedMasterPageClient : System.Web.UI.MasterPage
+    {
+        private Search s = new Search();
+        private String pageName;
+        private User curUser;
+        private List<PhotoshopWebsite.Domain.Photo> searchedPhotos;
+        private int size = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Session["SearchedPhotos"] = null;
+            }
+            if (Session["UserData"] != null)
+            {
+                curUser = Session["UserData"] as User;
+            }
+            if (HttpContext.Current.Session["shoppingCart"] != null)
+            {
+                List<Domain.ShoppingbasketItem> list = Session["shoppingCart"] as List<Domain.ShoppingbasketItem>;
+                foreach (ShoppingbasketItem item in list)
+                {
+                    size += item.quantity;
+                }
+                Labelquantity.Text = size.ToString();
+
+                //pageName = this.ContentPlaceHolder1.Page.GetType().FullName;
+                pageName = "";
+                Label LabelTitle = (Label)Master.FindControl("LabelTitle");
+                if (pageName.Contains("account"))
+                {
+
+                    LabelTitle.Text = "<h1>My Account</h1>";
+                }
+                else if (pageName.Contains("mainstore"))
+                {
+                    LabelTitle.Text = "<h1>My Pictures</h1>";
+                }
+                else if (pageName.Contains("group"))
+                {
+                    LabelTitle.Text = "<h1>Group Pictures</h1>";
+                }
+                else if (pageName.Contains("google"))
+                {
+                    LabelTitle.Text = "<h1>Google-Checkout Payment</h1>";
+                }
+                else if (pageName.Contains("ideal"))
+                {
+                    LabelTitle.Text = "<h1>iDeal Payment</h1>";
+                }
+                else if (pageName.Contains("ogone"))
+                {
+                    LabelTitle.Text = "<h1>Ogone Payment</h1>";
+                }
+                else if (pageName.Contains("transfer"))
+                {
+                    LabelTitle.Text = "<h1>Money Tranfer Payment</h1>";
+                }
+                else if (pageName.Contains("checkpayment"))
+                {
+                    LabelTitle.Text = "<h1>Payment Check</h1>";
+                }
+                else if (pageName.Contains("shoppingcart"))
+                {
+                    LabelTitle.Text = "<h1>Shopping Cart</h1>";
+                }
+            }
+        }
+        protected void Btnlogout_Click(object sender, EventArgs e)
+        {
+            Session["logindata"] = null;
+            Response.Redirect(Request.RawUrl);
+        }
+
+
+        protected void Btnsearch_Click(object sender, EventArgs e)
+        {
+            if (pageName.Contains("group"))
+            {
+                searchedPhotos = new List<Domain.Photo>();
+                if (tbSearch.Text != "")
+                {
+                    DataTable output = s.searchGroupPhoto(tbSearch.Text);
+                    PhotoshopWebsite.DatabaseTier.Photo photo = new PhotoshopWebsite.DatabaseTier.Photo();
+                    foreach (DataRow row in output.Rows)
+                    {
+                        List<String> data = photo.getPhoto(row[0].ToString());
+                        Domain.Photo newphoto = new Domain.Photo(Convert.ToInt32(data.ElementAt(0)), Convert.ToInt32(data.ElementAt(1)), Convert.ToInt32(data.ElementAt(2)), data.ElementAt(3), data.ElementAt(4), data.ElementAt(5));
+                        searchedPhotos.Add(newphoto);
+                    }
+                    Session["SearchedPhotos"] = searchedPhotos;
+                    Response.Redirect(Request.RawUrl);
+                }
+                else
+                {
+                    Response.Write("<script>alert('Voer zoekcriteria in')</script>");
+                }
+            }
+            else
+            {
+                searchedPhotos = new List<Domain.Photo>();
+                if (tbSearch.Text != "")
+                {
+                    DataTable output = s.searchPhoto(tbSearch.Text, curUser.ID);
+                    PhotoshopWebsite.DatabaseTier.Photo photo = new PhotoshopWebsite.DatabaseTier.Photo();
+                    foreach (DataRow row in output.Rows)
+                    {
+                        List<String> data = photo.getPhoto(row[0].ToString());
+                        Domain.Photo newphoto = new Domain.Photo(Convert.ToInt32(data.ElementAt(0)), Convert.ToInt32(data.ElementAt(1)), Convert.ToInt32(data.ElementAt(2)), data.ElementAt(3), data.ElementAt(4), data.ElementAt(5));
+                        searchedPhotos.Add(newphoto);
+                    }
+                    Session["SearchedPhotos"] = searchedPhotos;
+                    Response.Redirect(Request.RawUrl);
+                }
+                else
+                {
+                    Response.Write("<script>alert('Voer zoekcriteria in')</script>");
+                }
+            }
+        }
+    }
+}
