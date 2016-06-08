57634fbefd274d9cddd9107cd2aec3d2ecf16913
Charles-Foster-Kane <ramonj@live.nl>
2016-06-08 11:15:47 +0200

    CodeReview #10 processed

--- PhotoshopWebsite/Domain/LoginCode.cs
+++ PhotoshopWebsite/Domain/LoginCode.cs
@@ -5,6 +5,9 @@ using System.Web;
 
 namespace PhotoshopWebsite.Controller
 {
+    /// <summary>
+    /// LoginCode class is created for keeping track of logincodes that display photo's from the specific photographer
+    /// </summary>
     public class LoginCode
     {
         public int ID { get; set; }
@@ -13,6 +16,14 @@ namespace PhotoshopWebsite.Controller
         public int Used { get; set; }
         public DateTime ValidUntil { get; set; }
 
+        /// <summary>
+        /// create a LoginCode with all possible variables
+        /// </summary>
+        /// <param name="id">code part of the logincode</param>
+        /// <param name="mapID">map that belongs to the logincode</param>
+        /// <param name="superUserID">photographer that belongs to the map</param>
+        /// <param name="used">times used</param>
+        /// <param name="validUntil">experation date</param>
         public LoginCode(int id, int mapID, int superUserID, int used, DateTime validUntil)
         {
             this.ID = id;
