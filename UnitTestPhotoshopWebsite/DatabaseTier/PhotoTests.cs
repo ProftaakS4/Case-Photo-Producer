68999c9e5be56b36b185f7f1de09cb70d1120bbe
Charles-Foster-Kane <ramonj@live.nl>
2016-06-15 15:15:00 +0200

    databasetier photo replaced #12

--- UnitTestPhotoshopWebsite/DatabaseTier/PhotoTests.cs
+++ UnitTestPhotoshopWebsite/DatabaseTier/PhotoTests.cs
@@ -14,26 +14,25 @@ namespace PhotoshopWebsite.DatabaseTier.Tests
     public class PhotoTests
     {
         Photo photo = new Photo();
+        private DatabaseTier.QueryDatabase database = new DatabaseTier.QueryDatabase();
 
         [TestMethod()]
         public void getPhotosUserTest()
         {
-            DataTable getPhotosUser = photo.getPhotosUser(4);
-            Assert.IsNotNull(getPhotosUser);
-        }
 
-        [TestMethod()]
-        public void getTypesTest()
-        {
-            List<ProductTypes.PTypes> getTypes = photo.getTypes(1);
-            Assert.IsNotNull(getTypes);
+            Dictionary<string, string[]> parameters = new Dictionary<string, string[]>();
+            parameters.Add("p_id", new string[] { "int", 4.ToString() });
+            DataTable dt = database.CallProcedure("getPhotosUser", parameters);
+            Assert.IsNotNull(dt);
         }
 
         [TestMethod()]
         public void getPhotoTest()
         {
-            DataTable getPhoto = photo.getPhoto(4);
-            Assert.IsNotNull(getPhoto);
+            Dictionary<string, string[]> parameters = new Dictionary<string, string[]>();
+            parameters.Add("p_photo_ID", new string[] { "int", 4.ToString() });
+            DataTable dt = database.CallProcedure("getPhoto", parameters);
+            Assert.IsNotNull(dt);
         }
     }
 }
