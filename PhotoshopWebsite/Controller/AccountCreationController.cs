fe4064ead39ad645ebc3b0535db9537b81a4e422
Charles-Foster-Kane <ramonj@live.nl>
2016-06-15 14:25:16 +0200

    databasetier AccountCreation replaced #12

--- PhotoshopWebsite/Controller/AccountCreationController.cs
+++ PhotoshopWebsite/Controller/AccountCreationController.cs
@@ -8,7 +8,7 @@ namespace PhotoshopWebsite.Controller
 {
     public class AccountCreationController
     {
-        private DatabaseTier.AccountCreation DB = new DatabaseTier.AccountCreation();
+        private DatabaseTier.QueryDatabase database = new DatabaseTier.QueryDatabase();
 
         public AccountCreationController()
         {
@@ -16,19 +16,36 @@ namespace PhotoshopWebsite.Controller
         }
         public int getAccountId(string email)
         {
-            int id = DB.getIdByEmail(email);
-            return id;
+            Dictionary<string, string[]> parameters = new Dictionary<string, string[]>();
+            parameters.Add("p_email", new string[] { "string", email });
+            DataTable dt = database.CallProcedure("getAccountWithEmail", parameters);
+            return int.Parse(dt.Rows[0][0].ToString());
         }
 
         public Boolean insertAccountLoginCode(int accountId, int loginCode)
         {
-            bool value = DB.insertAccountLoginCode(accountId, loginCode);
-            return value;
+            Dictionary<string, string[]> parameters = new Dictionary<string, string[]>();
+            parameters.Add("p_account_ID", new string[] { "int", accountId.ToString() });
+            parameters.Add("p_logincode", new string[] { "int", loginCode.ToString() });
+            DataTable dt = database.CallProcedure("insertAccountLoginCode", parameters);
+            return dt.Rows.Count != 0;
         }
 
         public DataTable createAccountandgetinformation(string customer, string firstname, string lastname, string streetname, string housenumber, string zipcode, string city, string phonenumber, string iban, string emailadress, string password)
         {
-            DataTable dt = DB.createAccount(customer,firstname,lastname,streetname,housenumber,zipcode,city,phonenumber,iban,emailadress,password);
+            Dictionary<string, string[]> parameters = new Dictionary<string, string[]>();
+            parameters.Add("p_type", new string[] { "string", customer });
+            parameters.Add("p_firstname", new string[] { "string", firstname });
+            parameters.Add("p_Lastname", new string[] { "string", lastname });
+            parameters.Add("p_streetname", new string[] { "string", streetname });
+            parameters.Add("p_housenumber", new string[] { "string", housenumber });
+            parameters.Add("p_zipcode", new string[] { "string", zipcode });
+            parameters.Add("p_city", new string[] { "string", city });
+            parameters.Add("p_phonenumber", new string[] { "string", phonenumber });
+            parameters.Add("p_iban", new string[] { "string", iban });
+            parameters.Add("p_emailaddress", new string[] { "string", emailadress });
+            parameters.Add("p_password", new string[] { "string", password });
+            DataTable dt = database.CallProcedure("insertUser", parameters);
             return dt;
         }
     }
