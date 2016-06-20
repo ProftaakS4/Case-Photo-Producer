ab6d60ddba074d6dd3fa250e02ab16379fc765da
Charles-Foster-Kane <ramonj@live.nl>
2016-06-20 13:13:33 +0200

    insertPrintOrder replaced #12

--- PhotoshopWebsite/Controller/OrderController.cs
+++ PhotoshopWebsite/Controller/OrderController.cs
@@ -10,12 +10,12 @@ namespace PhotoshopWebsite.Controller
 {
     public class OrderController
     {
-        private DatabaseTier.Order DB = new DatabaseTier.Order();
+        private DatabaseTier.QueryDatabase database = new DatabaseTier.QueryDatabase();
 
         public int ID { get; set; }
         public List<Order> orders { get; set; }
         public List<OrderInfo> orderInfos { get; set; }
-        
+
         public OrderController(int accountID)
         {
             this.orders = this.getAllOrders(accountID);
@@ -26,33 +26,31 @@ namespace PhotoshopWebsite.Controller
             this.orderInfos = this.getAllOrderInfos(accountID, orderID);
         }
 
-        public List<Order>  getAllOrders(int accountID) {
-            
+        public List<Order> getAllOrders(int accountID)
+        {
             List<Order> temp = new List<Order>();
-            DataTable dt = DB.getAllOrders(accountID);
+            Dictionary<string, string[]> parameters = new Dictionary<string, string[]>();
+            parameters.Add("p_account_ID", new string[] { "int", accountID.ToString() });
+            DataTable dt = database.CallProcedure("getAllPaymentsByAccount", parameters);
             // when data is found and returned
-            if (dt.Rows.Count != 0)
+            foreach (DataRow data in dt.Rows)
             {
-                foreach (DataRow data in dt.Rows)
-                {
-                    temp.Add(new Order(int.Parse(data[0].ToString()), DateTime.Parse(data[1].ToString()), (PaymentType)Enum.Parse(typeof(PaymentType), data[2].ToString(), true), data[3].ToString(), double.Parse(data[4].ToString())));
-                }
+                temp.Add(new Order(int.Parse(data[0].ToString()), DateTime.Parse(data[1].ToString()), (PaymentType)Enum.Parse(typeof(PaymentType), data[2].ToString(), true), data[3].ToString(), double.Parse(data[4].ToString())));
             }
             return temp;
         }
 
         public List<OrderInfo> getAllOrderInfos(int accountID, int orderID)
         {
-
             List<OrderInfo> temp = new List<OrderInfo>();
-            DataTable dt = DB.getAllOrderInfos(accountID, orderID);
+            Dictionary<string, string[]> parameters = new Dictionary<string, string[]>();
+            parameters.Add("p_account_ID", new string[] { "int", accountID.ToString() });
+            parameters.Add("p_order_ID", new string[] { "int", orderID.ToString() });
+            DataTable dt = database.CallProcedure("getAllOrderInfos", parameters);
             // when data is found and returned
-            if (dt.Rows.Count != 0)
+            foreach (DataRow data in dt.Rows)
             {
-                foreach (DataRow data in dt.Rows)
-                {
-                    temp.Add(new OrderInfo(int.Parse(data[0].ToString()), data[1].ToString(), data[2].ToString(), data[3].ToString(), int.Parse(data[4].ToString())));
-                }
+                temp.Add(new OrderInfo(int.Parse(data[0].ToString()), data[1].ToString(), data[2].ToString(), data[3].ToString(), int.Parse(data[4].ToString())));
             }
             return temp;
         }
